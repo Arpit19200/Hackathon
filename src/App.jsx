06ee3cf2cb457416655494b3585d3ad0b023c2eb
Arpit19200 <rangiarpit2@gmail.com>
2024-12-30 21:09:20 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import SignUpForm from './components/signup/Signup'
 import OtpVerification from './components/OTPinput/otp/OtpVerification'
 import { NewPassword } from './components/Newpassword/auth/NewPassword'
+import Header from './components/header/header'
+import { Outlet } from 'react-router-dom'
 
 function App() {
   return (
     <>
-
-  <NewPassword/>
-
+    <Header/>
+    <Outlet/>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,11 @@ import SignUpForm from './components/signup/Signup.jsx'
 import LoginForm from './components/Sign-in/auth/LoginForm.jsx'
 import OtpVerification from './components/OTPinput/otp/OtpVerification.jsx'
 import { NewPassword } from './components/Newpassword/auth/NewPassword.jsx'
+import Header from './components/header/header.jsx'
+import { NewsSection } from './components/NewsPage/NewsSection.jsx'
+import NewsArticle from './components/NewsInfo/newsArticle/NewsArticle.jsx'
+import { JoinNetwork } from './components/CommunityPage/JoinNetwork.jsx'
+import { LeaderboardView } from './components/leaderboard/LeaderboardView.jsx'
 
 const route = createBrowserRouter([
   {
@@ -25,11 +30,33 @@ const route = createBrowserRouter([
   {
     path:"/ChangePassword",
     element:<NewPassword/>
+  },
+  {
+    path:"/Home",
+    element:<App/>,
+    children:[
+      {
+        path:"",
+        element:<NewsSection/>
+      },
+      {
+        path:"news",
+        element:<NewsArticle/>
+      },
+      {
+        path:"ranking",
+        element:<LeaderboardView/>
+      },
+      {
+        path:"community",
+        element:<JoinNetwork/>
+      }
+    ]
   }
 ])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={route}/>
+     <RouterProvider router={route}/> 
   </StrictMode>,
 )
