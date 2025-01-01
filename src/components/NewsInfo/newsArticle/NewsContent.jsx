1355e2a9d49d1172d13a3316214878ba21c434a9
Arpit19200 <rangiarpit2@gmail.com>
2025-01-01 17:13:46 +0530

    Delete src/components/NewsInfo/newsArticle directory

--- src/components/NewsInfo/newsArticle/NewsContent.jsx
+++
@@ -1,5 +0,0 @@
-import * as React from "react";
-
-export function NewsContent({ content }) {
-  return <div className="mt-2.5 max-md:max-w-full">{content}</div>;
-}
--- src/components/NewsInfo/newsArticle/NewsHeadline.jsx
+++
@@ -1,5 +0,0 @@
-import * as React from "react";
-
-export function NewsHeadline({ headline }) {
-  return <h1 className="font-semibold max-md:max-w-full">{headline}</h1>;
-}
